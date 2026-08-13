ac49fea6a22ed67e4a94fd1e62df0ec5175d45a3
c-carl <carlosvirla10@gmail.com>
2026-08-13 16:28:48 -0300

    feat: inica projeto de estudos em java

--- src/Fundamentos/Aula1/Main.java
+++ src/Fundamentos/Aula1/Main.java
@@ -1,4 +1,33 @@
 package Fundamentos.Aula1;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("============================================");
+        System.out.println("             Cadastro de usuário            ");
+        System.out.println("============================================");
+
+        System.out.print("Nome: ");
+        String username = sc.nextLine();
+
+        System.out.print("ID: ");
+        String id = sc.nextLine();
+
+        System.out.print("Cidade: ");
+        String city = sc.nextLine();
+
+        System.out.print("Idade: ");
+        int age = sc.nextInt();
+
+        System.out.println("============================================");
+        System.out.println("            Informações do usuário          ");
+        System.out.println("============================================");
+        System.out.println("Nome do usuário: " + username);
+        System.out.println("ID do usuário: " + id);
+        System.out.println("Cidade do usuário: " + city);
+        System.out.println("Idade do usuário: " + age + " anos\n");
+
+    }
 }
