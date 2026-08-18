b42b367e8327f166924f4aca36f08cfed51d3aca
c-carl <carlosvirla10@gmail.com>
2026-08-18 16:42:46 -0300

    feat: adiciona exercícios sobre switch

--- src/fundamentos/aula6/Main.java
+++ src/fundamentos/aula6/Main.java
@@ -1,4 +1,26 @@
 package fundamentos.aula6;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Em qual setor você trabalha ?");
+        var option = scanner.next();
+
+        switch (option){
+
+            case "ADM", "T.I":
+                System.out.println("Não é necessário utilizar equipamentos de segurança");
+                break;
+            case "ELETRICA", "MECANICA":
+                System.out.println("É necessário o uso de equipamentos de segurança\nSolicite ao coordenador que realize a compra dos equipamentos.");
+                break;
+            default:
+                System.out.println("Nenhum setor cadastrado.");
+        }
+
+        // Criar um passo a passo de conexão na rede usando switch
+    }
 }
--- src/fundamentos/aula6/Teste.java
+++ src/fundamentos/aula6/Teste.java
@@ -1,4 +1,37 @@
 package fundamentos.aula6;
+import java.util.Scanner;
 
 public class Teste {
+    public static void main(String[] args){
+        /*
+        * Fone E
+        * Fone D
+        * Celular
+        * Camisa
+        * Casaco
+        * TV
+        * Cadeira
+        */
+
+        var sc = new Scanner(System.in);
+
+        System.out.println("Qual produto você gostaria de buscar ?");
+        var option = sc.nextInt();
+
+        var message = switch (option){
+
+            case 1, 2 -> {
+                var side = option == 1 ? "Esquerdo do fone" : "Direito do fone";
+                yield String.format("Você escolheu o lado %s ", side);
+            }
+            case 3 -> "Celular";
+            case 4 -> "Camisa";
+            case 5 -> "Cadeira";
+            default -> "opção inválida";
+
+        };
+
+        System.out.println(message);
+
+    }
 }
